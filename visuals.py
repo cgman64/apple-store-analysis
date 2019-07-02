b95cfdb9a8a15ee9f5db1be6f537c88edb42bcd7
cgman64 <cguzman968@gmail.com>
2019-07-02 07:19:42 -0400

    modified visuals script, and implemented to results

--- visuals.py
+++ visuals.py
@@ -48,7 +48,7 @@ def distribution(data, transformed = False):
     fig.show()
 
 
-def evaluate(results, accuracy, f1):
+def evaluate(results):
     """
     Visualization code to display results of various learners.
     
@@ -60,7 +60,7 @@ def evaluate(results, accuracy, f1):
     """
   
     # Create figure
-    fig, ax = pl.subplots(2, 4, figsize = (11,7))
+    fig, ax = pl.subplots(2, 2, figsize = (11,7))
 
     # Constants
     bar_width = 0.3
@@ -68,10 +68,11 @@ def evaluate(results, accuracy, f1):
     
     # Super loop to plot four panels of data
     for k, learner in enumerate(results.keys()):
-        for j, metric in enumerate(['train_time', 'acc_train', 'f_train', 'pred_time', 'acc_test', 'f_test']):
+        for j, metric in enumerate(['train_time', 'r2score_train', 'pred_time', 'r2score_test']):
             for i in np.arange(3):
                 
                 # Creative plot code
+                print("atest")
                 ax[j//3, j%3].bar(i+k*bar_width, results[learner][i][metric], width = bar_width, color = colors[k])
                 ax[j//3, j%3].set_xticks([0.45, 1.45, 2.45])
                 ax[j//3, j%3].set_xticklabels(["1%", "10%", "100%"])
@@ -80,35 +81,36 @@ def evaluate(results, accuracy, f1):
     
     # Add unique y-labels
     ax[0, 0].set_ylabel("Time (in seconds)")
-    ax[0, 1].set_ylabel("Accuracy Score")
-    ax[0, 2].set_ylabel("F-score")
+    ax[0, 1].set_ylabel("R2 Score")
+    #ax[0, 2].set_ylabel("F-score")
     ax[1, 0].set_ylabel("Time (in seconds)")
-    ax[1, 1].set_ylabel("Accuracy Score")
-    ax[1, 2].set_ylabel("F-score")
+    ax[1, 1].set_ylabel("R2 Score")
+    #ax[1, 2].set_ylabel("F-score")
     
     # Add titles
     ax[0, 0].set_title("Model Training")
-    ax[0, 1].set_title("Accuracy Score on Training Subset")
-    ax[0, 2].set_title("F-score on Training Subset")
+    ax[0, 1].set_title("R2 Score on Training Subset")
+    #ax[0, 2].set_title("F-score on Training Subset")
     ax[1, 0].set_title("Model Predicting")
-    ax[1, 1].set_title("Accuracy Score on Testing Set")
-    ax[1, 2].set_title("F-score on Testing Set")
+    ax[1, 1].set_title("R2 Score on Testing Set")
+    #ax[1, 2].set_title("F-score on Testing Set")
     
     # Add horizontal lines for naive predictors
-    ax[0, 1].axhline(y = accuracy, xmin = -0.1, xmax = 3.0, linewidth = 1, color = 'k', linestyle = 'dashed')
-    ax[1, 1].axhline(y = accuracy, xmin = -0.1, xmax = 3.0, linewidth = 1, color = 'k', linestyle = 'dashed')
-    ax[0, 2].axhline(y = f1, xmin = -0.1, xmax = 3.0, linewidth = 1, color = 'k', linestyle = 'dashed')
-    ax[1, 2].axhline(y = f1, xmin = -0.1, xmax = 3.0, linewidth = 1, color = 'k', linestyle = 'dashed')
+    if False:
+	    ax[0, 1].axhline(y = accuracy, xmin = -0.1, xmax = 3.0, linewidth = 1, color = 'k', linestyle = 'dashed')
+	    ax[1, 1].axhline(y = accuracy, xmin = -0.1, xmax = 3.0, linewidth = 1, color = 'k', linestyle = 'dashed')
+	    ax[0, 2].axhline(y = f1, xmin = -0.1, xmax = 3.0, linewidth = 1, color = 'k', linestyle = 'dashed')
+	    ax[1, 2].axhline(y = f1, xmin = -0.1, xmax = 3.0, linewidth = 1, color = 'k', linestyle = 'dashed')
     
     # Set y-limits for score panels
     ax[0, 1].set_ylim((0, 1))
-    ax[0, 2].set_ylim((0, 1))
+    #ax[0, 2].set_ylim((0, 1))
     ax[1, 1].set_ylim((0, 1))
-    ax[1, 2].set_ylim((0, 1))
+    #ax[1, 2].set_ylim((0, 1))
 
     # Set additional plots invisibles
-    ax[0, 3].set_visible(False)
-    ax[1, 3].axis('off')
+    ax[0, 2].set_visible(False)
+    ax[1, 2].axis('off')
 
     # Create legend
     for i, learner in enumerate(results.keys()):
